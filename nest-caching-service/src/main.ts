9e9b18560f64d38f37f2a71784921dba939c1476
Brayden Wong <braydenwong.code@gmail.com>
2023-05-13 14:23:26 -0600

    here is the example branch

--- nest-caching-service/src/main.ts
+++ nest-caching-service/src/main.ts
@@ -7,19 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const MS_PORT = configService.get<number>('MS');
-  const ms = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: MS_PORT,
-        host: 'whatsapp-service',
-        retryAttempts: 5,
-        retryDelay: 1000,
-      },
-    },
-  );
-  await ms.listen();
+
+  await app.startAllMicroservices();
   const PORT = configService.get<number>('PORT');
   await app.listen(PORT);
 }
